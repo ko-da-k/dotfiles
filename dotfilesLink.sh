0065b9f84d6e12a1555ab4c98ef98d53a23e5a40
Kodai Katoh <Kodai@Kotori.local>
2016-05-29 17:21:06 +0900

    add C#

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vim/dein/dein.toml ~/.vim/dein/dein.toml
 ln -sf ~/dotfiles/.vim/dein/dein_lazy.toml ~/.vim/dein/dein_lazy.toml
+if [ ! -e ~/.vim/ftplugin ]; then
+    mkdir ~/.vim/ftplugin
+fi
 ln -sf ~/dotfiles/.vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
