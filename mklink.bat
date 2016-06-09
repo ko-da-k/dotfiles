474fa2ed6e0e7003b42e549c95582a40ac79e056
Kodai Katoh <Kodai@Kotori.local>
2016-06-09 20:20:22 +0900

    fixed

--- mklink.bat
+++ mklink.bat
@@ -1,2 +1,5 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.vim\dein\dein.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein.toml"
+mklink %HOMEPATH%"\.vim\dein\dein_lazy.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein_lazy.toml"
+mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
 exit 0
