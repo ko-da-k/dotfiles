eabe7e42ff923c4d88dd605e0035467523eab446
Kodai Katoh <Kodai@Kotori.local>
2016-10-25 20:41:06 +0900

    fixed

---
+++ .bashrc
@@ -0,0 +1,3 @@
+alias rmdockervolume = 'docker volume rm $(docker volume ls -qf dangling=true)'
+alias b = 'cd ..'
+
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,14 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+
 ln -sf ~/dotfiles/.vim/dein/dein.toml ~/.vim/dein/dein.toml
 ln -sf ~/dotfiles/.vim/dein/dein_lazy.toml ~/.vim/dein/dein_lazy.toml
+
 if [ ! -e ~/.vim/ftplugin ]; then
     mkdir ~/.vim/ftplugin
 fi
 ln -sf ~/dotfiles/.vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+
