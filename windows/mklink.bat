44648dce8da22c6897bbcefbe16088602b41a935
Kodai Kato <di.aps53@gmail.com>
2016-11-19 20:41:27 +0900

    remove bash_profile link

--- windows/mklink.bat
+++ windows/mklink.bat
@@ -1,7 +1,6 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
 mklink %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
-mklink %HOMEPATH%"\.bash_profile" %HOMEPATH%"\dotfiles\windows\.bash_profile"
 
 mklink %HOMEPATH%"\.vim\dein\dein.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein.toml"
 mklink %HOMEPATH%"\.vim\dein\dein_lazy.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein_lazy.toml"
