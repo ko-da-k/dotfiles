6887ca0fbc042b542f0ec5509a5f4bc1233a89a9
Kodai Kato <di.aps53@gmail.com>
2016-11-19 20:12:33 +0900

    add link

--- windows/mklink.bat
+++ windows/mklink.bat
@@ -1,5 +1,8 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.bash_profile" %HOMEPATH%"\dotfiles\.bash_profile"
+mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
+mklink %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
+
 mklink %HOMEPATH%"\.vim\dein\dein.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein.toml"
 mklink %HOMEPATH%"\.vim\dein\dein_lazy.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein_lazy.toml"
-mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
 exit 0
