a78742f1a9b1c2997dbca4a260c6a8bed056a780
lab-desktop <di.aps53@gmail.com>
2017-05-29 14:32:04 +0900

    add vsvimrc

--- dotfilesLink_windows.sh
+++ dotfilesLink_windows.sh
@@ -1,1 +1,2 @@
 ln -sf ~/dotfiles/windows/.bash_profile ~/.bash_profile 
+ln -sf ~dotfiles/windows/.vsvimrc ~./.vsvimrc
--- windows/mklink.bat
+++ windows/mklink.bat
@@ -4,6 +4,7 @@ mklink %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
 mklink %HOMEPATH%"\.zsh_profile" %HOMEPATH%"\dotfiles\windows\.zsh_profile"
 mklink %HOMEPATH%"\.zshenv" %HOMEPATH%"\dotfiles\.zshenv"
 mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
+mklink %HOMEPATH%"\.vsvimrc" %HOMEPATH%"\dotfiles\windows\.vsvimrc"
 
 mklink %HOMEPATH%"\.vim\dein\dein.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein.toml"
 mklink %HOMEPATH%"\.vim\dein\dein_lazy.toml" %HOMEPATH%"\dotfiles\.vim\dein\dein_lazy.toml"
