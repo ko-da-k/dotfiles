f1eeec2a0fc483e3aefe9f0b4bf106e1e9c4dce3
Kodai Kato <kodaikato@RLS01024182M01.local>
2018-04-06 10:14:27 +0900

    renamed .zsh_profile -> .zprofile

--- dotfiles_shell.sh
+++ dotfiles_shell.sh
@@ -2,6 +2,6 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
 
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
