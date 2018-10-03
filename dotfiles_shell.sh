136bf3b7a4994120d7ae90b3f866710679a8d964
Kodai Kato <di.aps53@gmail.com>
2018-10-03 16:37:14 +0900

    fixed zprofile

--- dotfiles_shell.sh
+++ dotfiles_shell.sh
@@ -2,6 +2,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.bash_profile ~/.zprofile # because .zprofile is same as .bash_profile.
 
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/.pycharmrc ~/.pycharmrc
