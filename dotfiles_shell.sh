85b6113494aa67934a8fa70c1d03381853714353
Kodai Kato <di.aps53@gmail.com>
2018-10-23 15:15:18 +0900

    add tmux

--- dotfiles_shell.sh
+++ dotfiles_shell.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bash_profile ~/.zprofile # because .zprofile is same as .bash_profile.
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.pycharmrc ~/.pycharmrc
