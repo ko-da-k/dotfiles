1d1dcaec2f3c01b2cd1515abea73b1cc70b436fb
Kodai Kato <di.aps53@gmail.com>
2018-12-13 23:19:26 +0900

    refactoring

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
