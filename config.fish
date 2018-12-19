7378cfde78a3a067e36cfb694c278ae583460ba4
Kodai Kato <di.aps53@gmail.com>
2018-12-19 21:38:51 +0900

    add go111

--- config.fish
+++ config.fish
@@ -1,8 +1,13 @@
-alias g git
+alias g gcloud
+alias k kubectl
+
+set fish_plugins theme peco
 
 if test -d ~/.pyenv
   set -x PYENV_ROOT $HOME/.pyenv
-  set -x PATH $PYENV_ROOT/bin $PATH
+  if test -d ~/.pyenv/bin
+    set -x PATH $PYENV_ROOT/bin $PATH
+  end
   eval (pyenv init - | source)
 end
 
@@ -10,14 +15,18 @@ if test -d ~/.nodebrew
   set -x PATH $HOME/.nodebrew/current/bin $PATH
 end
 
-if test -d ~/.go
-  set -x GOROOT $HOME/.go
-  set -x GOPATH $HOME/Document/code/go
-  set -x PATH $GOROOT/bin $PATH
-end
-
 if test -d ~/.linuxbrew
   eval ($HOME/.linuxbrew/bin/brew shellenv | source)
   eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv | source)
   set -x PATH $HOME/.linuxbrew/bin $PATH
 end
+
+set -x GOROOT /usr/local/opt/go/libexec
+set -x GOPATH $HOME/go
+if test -d $GOPATH/bin
+  set -x PATH $GOPATH/bin $PATH
+end
+set -x PATH $GOROOT/bin $PATH
+set -x GO111MODULE on
+
+
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings insert
+    bind \cr 'peco_select_history (commandline -b)'
 end
 
 function fish_mode_prompt
