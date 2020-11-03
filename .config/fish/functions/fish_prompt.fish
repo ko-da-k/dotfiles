720b819798b9ce928509985995d43221f78a24d9
ko-da-k <di.aps53@gmail.com>
2020-11-03 19:31:29 +0900

    update fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -7,53 +7,64 @@ end
 function fish_mode_prompt
 end
 
-function fish_prompt
-	test $SSH_TTY
-    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-    test "$USER" = 'root'
-    and echo (set_color red)"#"
+function fish_prompt 
+    set -l last_status $status
+    if test $last_status -eq 0
+        set laststatus ''
+    else
+        set laststatus (echo -n (set_color brred)'err'$status':')
+    end
 
-    set laststatus $status
     function _git_branch_name
         echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
     end
     function _is_git_dirty
         echo (git status -s --ignore-submodules=dirty ^/dev/null)
     end
-    if [ (_git_branch_name) ]
-        set -l git_branch (set_color -o blue)(_git_branch_name)
-        if [ (_is_git_dirty) ]
-            for i in (git branch -qv --no-color | string match -r '\*' | cut -d' ' -f4- | cut -d] -f1 | tr , \n)\
- (git status --porcelain | cut -c 1-2 | uniq)
-                switch $i
-                    case "*[ahead *"
-                        set git_status "$git_status"(set_color red)↑
-                    case "*behind *"
-                        set git_status "$git_status"(set_color red)↓
-                    case "."
-                        set git_status "$git_status"(set_color green)＋
-                    case " D"
-                        set git_status "$git_status"(set_color red)×
-                    case "*M*"
-                        set git_status "$git_status"(set_color green)＊
-                    case "*R*"
-                        set git_status "$git_status"(set_color purple)→
-                    case "*U*"
-                        set git_status "$git_status"(set_color brown)＝
-                    case "??"
-                        set git_status "$git_status"(set_color red)≠
+    function _git_info
+        if [ (_git_branch_name) ]
+            set -l git_branch (set_color -o blue)(_git_branch_name)
+            if [ (_is_git_dirty) ]
+                for i in (git branch -qv --no-color | string match -r '\*' | cut -d' ' -f4- | cut -d] -f1 | tr , \n)\
+                (git status --porcelain | cut -c 1-2 | uniq)
+                    switch $i
+                        case "*[ahead *"
+                            set git_status "$git_status"(set_color red)↑
+                        case "*behind *"
+                            set git_status "$git_status"(set_color red)↓
+                        case "."
+                            set git_status "$git_status"(set_color green)＋
+                        case " D"
+                            set git_status "$git_status"(set_color red)×
+                        case "*M*"
+                            set git_status "$git_status"(set_color green)＊
+                        case "*R*"
+                            set git_status "$git_status"(set_color purple)→
+                        case "*U*"
+                            set git_status "$git_status"(set_color brown)＝
+                        case "??"
+                            set git_status "$git_status"(set_color red)≠
+                    end
                 end
+            else
+                set git_status (set_color green):
             end
-        else
-            set git_status (set_color green):
+            echo "(git$git_status$git_branch"(set_color white)")"
         end
-        set git_info "(git$git_status$git_branch"(set_color white)")"
-    end
+    end 
+
+    function _kubectx
+        if type kubectl > /dev/null 2>&1
+            echo (kubectl config current-context)
+        else
+            echo '-'
+        end
+    end 
 
     # Main
-    echo -n (set_color cyan)(prompt_pwd)(set_color white) $git_info (set_color red)'>'(set_color yellow)'>'(set_color green)'> '
+    echo -n $laststatus(set_color brpurple)'['(date "+%H:%M:%S")']' (set_color brcyan)(prompt_pwd)(set_color brwhite) (_git_info)(set_color brcyan) '[kubectx:'(_kubectx)']' \n(set_color brred)'>'(set_color bryellow)'>'(set_color brgreen)'> '
 end
 
 function fish_right_prompt
-    echo -n '['(date "+%H:%M:%S")']'
+    # intentionally left blank
 end
