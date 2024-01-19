753ae8fa0dba26be1c93b5176d1e1888a783fe83
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2024-01-19 20:18:18 +0900

    add nushell history search

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -12,5 +12,33 @@
       lla = "ls -la";
       gco = "git checkout";
     };
+
+    extraConfig = ''
+      $env.config = {
+        keybindings: [
+          {
+            name: fuzzy_history
+            modifier: control
+            keycode: char_r
+            mode: [emacs, vi_normal, vi_insert]
+            event: [
+              {
+                send: ExecuteHostCommand
+                cmd: "commandline (
+                  history 
+                    | each { |it| $it.command } 
+                    | uniq 
+                    | reverse 
+                    | str join (char -i 0) 
+                    | fzf --read0 --layout=reverse --height=40% -q (commandline) 
+                    | decode utf-8 
+                    | str trim
+                )"
+              }
+            ]
+          }
+        ]
+      }
+    '';
   };
 }
