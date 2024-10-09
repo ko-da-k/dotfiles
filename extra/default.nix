59ed51bc96dbd6634d83f41da34a03aaca415125
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2024-10-09 20:38:23 +0900

    rm hyper

--- extra/default.nix
+++ extra/default.nix
@@ -2,9 +2,6 @@
 { config, pkgs, ... }:
 
 {
-  home.file = {
-    ".hyper.js".source = ./.hyper.js;
-  };
   xdg.configFile."alacritty/dracula.toml".source = ./.config/alacritty/dracula.toml;
   xdg.configFile."alacritty/alacritty.toml".source = ./.config/alacritty/alacritty.toml;
   xdg.configFile."git/ignore".source = ./.config/git/ignore;
