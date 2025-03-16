5a388106d5887ce7f35edc885c6c4b1f7c41ae9e
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2025-03-16 14:00:01 +0900

    add insiders

--- extra/vscode/sync.sh
+++ extra/vscode/sync.sh
@@ -7,7 +7,14 @@ set -eu -o pipefail -x
 
 cd $(dirname "$0")
 
+# Configure regular VSCode
 vscode_config_dir="$HOME/Library/Application Support/Code/User"
-
+mkdir -p "$vscode_config_dir"
 ln -sf "$(pwd)/settings.json" "$vscode_config_dir/settings.json"
 ln -sf "$(pwd)/keybindings.json" "$vscode_config_dir/keybindings.json"
+
+# Configure VSCode Insiders
+vscode_insiders_config_dir="$HOME/Library/Application Support/Code - Insiders/User"
+mkdir -p "$vscode_insiders_config_dir"
+ln -sf "$(pwd)/settings.json" "$vscode_insiders_config_dir/settings.json"
+ln -sf "$(pwd)/keybindings.json" "$vscode_insiders_config_dir/keybindings.json"
