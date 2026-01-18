0ed21e858f96b2aba93762678d1f6f3170748313
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2026-01-18 10:14:57 +0900

    feat: zellij config

--- home/terminal/zellij.nix
+++ home/terminal/zellij.nix
@@ -11,6 +11,27 @@
       show_startup_tips = false;
     };
 
+    layouts = {
+      default = ''
+      layout {
+        default_tab_template {
+          pane size=1 borderless=true {
+            plugin location="zellij:tab-bar"
+          }
+          children
+          pane size=2 borderless=true {
+            plugin location="zellij:status-bar"
+          }
+        }
+        swap_floating_layout {
+          floating_panes max_panes=1 {
+            pane x="5%" y="5%" width="90%" height="90%"
+          }
+        }
+      }
+      '';
+    };
+
     extraConfig = ''
       keybinds {
         locked {
