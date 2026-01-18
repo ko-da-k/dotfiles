2705ee2c72011111f4dca38389a51e23082ce147
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2026-01-18 11:10:16 +0900

    feat(zellij): add keybind in lock mode

--- home/terminal/zellij.nix
+++ home/terminal/zellij.nix
@@ -35,7 +35,35 @@
     extraConfig = ''
       keybinds {
         locked {
+          // Floating panes
           bind "Alt f" { ToggleFloatingPanes; }
+          
+          // New tab
+          bind "Alt t" { NewTab; }
+          
+          // New pane
+          bind "Alt n" { NewPane; }
+          bind "Alt N" { NewPane "Down"; }
+          
+          // Close pane
+          bind "Alt w" { CloseFocus; }
+          
+          // Move focus between panes
+          bind "Alt Left" { MoveFocus "Left"; }
+          bind "Alt Right" { MoveFocus "Right"; }
+          bind "Alt Up" { MoveFocus "Up"; }
+          bind "Alt Down" { MoveFocus "Down"; }
+          
+          // Go to tab by number
+          bind "Alt 1" { GoToTab 1; }
+          bind "Alt 2" { GoToTab 2; }
+          bind "Alt 3" { GoToTab 3; }
+          bind "Alt 4" { GoToTab 4; }
+          bind "Alt 5" { GoToTab 5; }
+          bind "Alt 6" { GoToTab 6; }
+          bind "Alt 7" { GoToTab 7; }
+          bind "Alt 8" { GoToTab 8; }
+          bind "Alt 9" { GoToTab 9; }
         }
       }
     '';
